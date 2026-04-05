e52304dfd691fee83fb1291aba8aae2f5464b695
Shubham Dhokare <shubhamdhokare01@gmail.com>
2026-04-05 19:13:17 +0530

    version 1.1

--- backend/src/main/java/com/kanban/kanban_backend/KanbanBackendApplication.java
+++ backend/src/main/java/com/kanban/kanban_backend/KanbanBackendApplication.java
@@ -1,13 +1,25 @@
 package com.kanban.kanban_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KanbanBackendApplication {
 
+	static {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+		
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanBackendApplication.class, args);
 	}
-
 }
